9cc208ddead82f7c02b19f888dbc29ba480353b2
shashi kisku <shashi.kisku@bridgesgi.com>
2022-08-08 00:01:38 +0530

    servletConfig,servletContext

--- Phase2Code/WebProjectMaven/src/main/java/com/simplilearn/phase2/servlets/HelloServlet.java
+++ Phase2Code/WebProjectMaven/src/main/java/com/simplilearn/phase2/servlets/HelloServlet.java
@@ -3,6 +3,8 @@ package com.simplilearn.phase2.servlets;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -11,11 +13,26 @@ import javax.servlet.http.HttpServletResponse;
 public class HelloServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		PrintWriter writer=response.getWriter();
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		PrintWriter writer = response.getWriter();
 		writer.println("Hello in BROWSer");
+
+		// server will automatically create object, we are retrieving it.
+		ServletConfig config = getServletConfig();
+		String uname = config.getInitParameter("username");
+		String pass = config.getInitParameter("password");
+		System.out.println("username: " + uname + " and password : " + pass);
+
+		// universal value across application/all servlet
+		ServletContext context = getServletContext();
+		String driver = context.getInitParameter("driver");
+		String url = context.getInitParameter("url");
+		System.out.println(driver + ":" + url);
 	}
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		doGet(request, response);
 	}
 
